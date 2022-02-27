7851d21bd0841c5ce903132e42786530d01421bb
unknown <thunderrk2001@gmail.com>
2022-02-27 23:10:09 +0530

    issue

--- client/js/homePage.js
+++ client/js/homePage.js
@@ -43,3 +43,83 @@ async function getConnectedAccount() {
         console.log(accounts)
     }
 }
+setTimeout(async() => { callF() }, 3000)
+async function callF() {
+    var contractAbi = `[
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "funds",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_manager",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_fundName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_desc",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_minAmount",
+          "type": "uint256"
+        }
+      ],
+      "name": "createFund",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getFundsDetails",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+  ]` //here put your contract abi in json string
+    const web_3 = new window.Web3("HTTP://127.0.0.1:7545");
+    const contract = new web_3.eth.Contract(JSON.parse(contractAbi), "0xcE9716aAA5B7Ccff304A3Eb1d7fFEb228233b785", {
+        from: '0x464Ff25A4A6bB9E00F2D4B0E3454c1Db83788375',
+        gasPrice: '20000000000'
+    });
+    console.log(contract.methods)
+    try {
+        const res = await contract.methods.createFund("first", "hello", 2).send({ from: '0x464Ff25A4A6bB9E00F2D4B0E3454c1Db83788375' });
+        console.log(res);
+    } catch (e) {
+        console.log(e)
+    }
+
+}
--- contracts/FundFactory.sol
+++ contracts/FundFactory.sol
@@ -3,8 +3,8 @@ import './Fund.sol';
 pragma solidity >=0.4.22 <0.9.0;
 contract FundFactory{
   address[] public funds;
-  function createFund(address _manager,string  memory _fundName,string memory _desc,uint  _minAmount) public{
-    funds.push(address(new Fund(_manager,_fundName,_desc,_minAmount)));
+  function createFund(string  memory _fundName,string memory _desc,uint  _minAmount) public{
+    funds.push(address(new Fund(address(msg.sender),_fundName,_desc,_minAmount)));
   }
   function getFundsDetails() public view returns(address [] memory){
     return funds;
